7f6ab5995c49d526f23fcae92b303e3322382106
Alex Mubarakshin <muban96@mail.ru>
2024-12-16 20:56:15 +0200

    🏃 Adding github runner

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+declare const process: {
+  env: {
+    APP_BASE_PATH: string | undefined;
+  };
+};
+
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath:
+    process.env.APP_BASE_PATH && typeof process.env.APP_BASE_PATH === "string"
+      ? process.env.APP_BASE_PATH
+      : undefined,
 };
 
 export default nextConfig;
